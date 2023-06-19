f1a2a8d1fdfd0021e54787926a2ed0f1c6e28ea7
Niko Leinonen <niko.a.leinonen@gmail.com>
2023-06-19 10:39:14 +0300

    Port code hotspots into Cython

--- nalpy/math/__init__.py
+++ nalpy/math/__init__.py
@@ -37,6 +37,7 @@ from math import isnan as isnan
 from math import isfinite as isfinite
 
 # Value manipulation
+from math import trunc as trunc
 from math import modf as modf
 from math import nextafter as nextafter
 
@@ -44,6 +45,25 @@ from math import nextafter as nextafter
 from math import gcd as gcd
 from math import lcm as lcm
 
+#endregion
+
+#region public imports of c extension functions
+from nalpy.math._c_extensions.functions import round as round
+from nalpy.math._c_extensions.functions import remap as remap
+from nalpy.math._c_extensions.functions import remap01 as remap01
+from nalpy.math._c_extensions.functions import clamp as clamp
+from nalpy.math._c_extensions.functions import clamp01 as clamp01
+from nalpy.math._c_extensions.functions import delta_angle as delta_angle
+from nalpy.math._c_extensions.functions import sign as sign
+
+from nalpy.math._c_extensions.functions import lerp as lerp
+from nalpy.math._c_extensions.functions import lerp_unclamped as lerp_unclamped
+from nalpy.math._c_extensions.functions import lerp_angle as lerp_angle
+from nalpy.math._c_extensions.functions import inverse_lerp as inverse_lerp
+from nalpy.math._c_extensions.functions import smooth_step as smooth_step
+from nalpy.math._c_extensions.functions import move_towards as move_towards
+from nalpy.math._c_extensions.functions import move_towards_angle as move_towards_angle
+from nalpy.math._c_extensions.functions import ping_pong as ping_pong
 #endregion
 
 # Public component imports at the bottom
@@ -90,21 +110,6 @@ NOTE: Python integers don't have a minimum value.
 _NumberT = __typing.TypeVar("_NumberT", int, float)
 
 #region Basic math functions
-def sign(__x: __typing.SupportsFloat) -> int:
-    """
-    A number that indicates the sign of ``__x``.
-
-    ```
-    -1 | less than zero
-     0 | equal to zero
-    +1 | greater than zero
-    ```
-    """
-    return bool(float(__x) > 0.0) - bool(float(__x) < 0.0)
-    # return int(math.copysign(1.0, __x))
-    # copysign behaviour is not consistent across different platforms
-
-
 def is_positive_inf(__x: __typing.SupportsFloat) -> bool:
     """Return ``True`` if ``x`` is positive infinity, and ``False`` otherwise."""
     x = float(__x)
@@ -114,57 +119,9 @@ def is_negative_inf(__x: __typing.SupportsFloat) -> bool:
     """Return ``True`` if ``x`` is negative infinity, and ``False`` otherwise."""
     x = float(__x)
     return isinf(x) and x < 0 # faster than f == NEGATIVE_INFINITY, supposedly
-
-def delta_angle(current: float, target: float) -> float:
-    """Calculates the shortest difference between two given angles."""
-    delta = (target - current) % 360.0
-    if delta > 180.0:
-        delta -= 360.0
-    return delta
-#endregion
-
-#region Value Manipulation
-def clamp(value: _NumberT, _min: _NumberT, _max: _NumberT) -> _NumberT:
-    """Clamps the value to the specified range. Both ends are inclusive."""
-    if _min > _max:
-        raise ValueError("Minimum value cannot be over the maximum value.")
-    return max(_min, min(value, _max))
-
-def clamp01(value: float) -> float:
-    """Shorthand for `math.clamp(value, 0.0, 1.0)`"""
-    return max(0.0, min(value, 1.0)) # Skips min max range check
-
-
-def remap(value: float, from1: float, to1: float, from2: float, to2: float) -> float:
-    """Converts a value to another value within the given arguments. Ends are inclusive."""
-    return (value - from1) / (to1 - from1) * (to2 - from2) + from2
-
-def remap01(value: float, from1: float, to1: float) -> float:
-    """Converts a value to another value within 0.0 and 1.0. Ends are inclusive."""
-    return (value - from1) / (to1 - from1)
 #endregion
 
 #region Rounding
-#region Away from zero
-def round_away_from_zero(__x: __typing.SupportsFloat) -> int:
-    """
-    Round a number to an integer.
-    When a number is halfway between two others, it's rounded toward the nearest number that's away from zero.
-    """
-    if isnan(__x) or isinf(__x):
-        raise ValueError(f"Cannot round value: '{__x}'")
-
-    # Split integral and fractional parts
-    fraction, float_value = modf(__x)
-    value = int(float_value)
-
-    # Rounding
-    if abs(fraction) >= 0.5:
-        value += sign(fraction)
-
-    return value
-#endregion
-
 #region Nearest n
 def round_to_nearest_n(__x: __typing.SupportsFloat, n: int) -> int:
     """
@@ -173,7 +130,7 @@ def round_to_nearest_n(__x: __typing.SupportsFloat, n: int) -> int:
     """
     if n == 0: # Prevent division by zero. Any multiple of zero is always zero.
         return 0
-    return round_away_from_zero(float(__x) / n) * n
+    return round(float(__x) / n) * n
 
 def floor_to_nearest_n(__x: __typing.SupportsFloat, n: int) -> int:
     """Floor a number to the nearest multiple of ``n``."""
@@ -200,7 +157,7 @@ def floor_to_digits(__x: __typing.SupportsFloat, digits: int = 0) -> float:
     return floor(float(__x) * pow10) / pow10 # dividing changes output to float
 
 
-def round_away_from_zero_to_digits(__x: __typing.SupportsFloat, digits: int = 0) -> float:
+def round_to_digits(__x: __typing.SupportsFloat, digits: int = 0) -> float:
     """
     Round a number to a given precision in decimal digits.
     When a number is halfway between two others, it's rounded toward the nearest number that's away from zero.
@@ -208,7 +165,7 @@ def round_away_from_zero_to_digits(__x: __typing.SupportsFloat, digits: int = 0)
     ``digits`` may be negative.
     """
     pow10: float = pow(10.0, digits)
-    return round_away_from_zero(float(__x) * pow10) / pow10 # dividing changes output to float
+    return round(float(__x) * pow10) / pow10 # dividing changes output to float
 
 
 def round_to_nearest_n_to_digits(__x: __typing.SupportsFloat, n: int, digits: int = 0) -> float:
@@ -226,81 +183,6 @@ def ceil_to_nearest_n_to_digits(__x: __typing.SupportsFloat, n: int, digits: int
     pow10: float = pow(10.0, digits)
     return ceil_to_nearest_n(float(__x) * pow10, n) / pow10 # dividing changes output to float
 #endregion
-#endregion
-
-#region Interpolation
-def lerp(a: float, b: float, t: float) -> float:
-    """
-    Linearly interpolates between ``a`` and ``b`` by ``t``.
-
-    The parameter ``t`` is clamped to the range [0, 1].
-    """
-    return a + (b - a) * clamp01(t)
-
-def lerp_unclamped(a: float, b: float, t: float) -> float:
-    """
-    Linearly interpolates between ``a`` and ``b`` by ``t``.
-
-    The parameter ``t`` is not clamped.
-    """
-    return a + (b - a) * t
-
-def lerp_angle(a: float, b: float, t: float) -> float:
-    """
-    Same as ``lerp``, but makes sure the values interpolate correctly when they wrap around 360 degrees.
-
-    The parameter t is clamped to the range [0, 1]. Variables a and b are assumed to be in degrees.
-    """
-    return a + delta_angle(a, b) * clamp01(t)
-
-def inverse_lerp(a: float, b: float, value: float) -> float:
-    """Calculates the clamped parameter ``t`` in ``lerp`` when output is the given ``value``."""
-    if a != b:
-        return clamp01((value - a) / (b - a))
-    else:
-        return 0.0
-
-def smooth_step(a: float, b: float, t: float) -> float:
-    """
-    Smoothly interpolates between a and b by t.
-
-    The parameter t is clamped to the range [0, 1].
-    """
-    t = clamp01(t)
-    t = -2.0 * t * t * t + 3.0 * t * t
-    return b * t + a * (1 - t)
-
-def move_towards(current: float, target: float, maxDelta: float) -> float:
-    """
-    Moves a value current towards target.
-
-    This is essentially the same as Lerp, but instead the function will ensure that the speed never exceeds maxDelta. Negative values of maxDelta pushes the value away from target.
-    """
-    if abs(target - current) <= maxDelta:
-        return target
-    return current + sign(target - current) * maxDelta
-
-def move_towards_angle(current: float, target: float, maxDelta: float) -> float:
-    """
-    Same as MoveTowards but makes sure the values interpolate correctly when they wrap around 360 degrees.
-
-    Variables current and target are assumed to be in degrees. For optimization reasons, negative values of maxDelta are not supported and may cause oscillation. To push current away from a target angle, add 180 to that angle instead.
-    """
-
-    deltaAngle = delta_angle(current, target)
-    if -maxDelta < deltaAngle and deltaAngle < maxDelta:
-        return target
-    target = current + deltaAngle
-    return move_towards(current, target, maxDelta)
-
-def ping_pong(t: float, length: float) -> float:
-    """
-    Returns a value that will increment and decrement between the value 0 and length.
-
-   ``t`` has to be a self-incrementing value.
-    """
-    t = t % (length * 2.0)
-    return length - abs(t - length)
 #endregion
 
 #region Iterables
--- setup.py
+++ setup.py
@@ -3,6 +3,7 @@ from setuptools import setup, Extension, find_packages
 setup(
     packages=find_packages(where=".", include=["nalpy*"]),
     ext_modules=[
+        Extension("nalpy.math._c_extensions.functions", ["nalpy/math/_c_extensions/functions.c"]),
         Extension("nalpy.math._c_extensions.vector2", ["nalpy/math/_c_extensions/vector2.c"]),
         Extension("nalpy.math._c_extensions.vector2_int", ["nalpy/math/_c_extensions/vector2_int.c"])
     ],
